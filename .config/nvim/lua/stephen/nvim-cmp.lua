07024a025493942bfaa88339146e12db0c9b8cdc
Stephen Reynolds <mail@stephenreynolds.dev>
2022-11-01 11:52:33 -0400

    Add nvim configs

---
+++ .config/nvim/lua/stephen/nvim-cmp.lua
@@ -0,0 +1,45 @@
+local cmp = require('cmp')
+local lspkind = require('lspkind')
+
+cmp.setup {
+    mapping = {
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<c-y>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true
+        },
+        ['<c-space>'] = cmp.mapping.complete()
+    },
+    sources = {
+        { name = 'gh_issues' },
+        { name = 'nvim_lua' },
+        { name = 'nvim_lsp' },
+        { name = 'path' },
+        { name = 'luasnip' },
+        { name = 'buffer', keyword_length = 5 }
+    },
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end
+    },
+    formatting = {
+        format = lspkind.cmp_format {
+            with_text = true,
+            menu = {
+                buffer = '[buf]',
+                nvim_lsp = '[LSP]',
+                nvim_lua = '[api]',
+                path = '[path]',
+                luasnip = '[snip]',
+                gh_issues = '[issues]'
+            }
+        }
+    },
+    experimental = {
+        native_menu = false,
+        ghost_text = true
+    }
+}
