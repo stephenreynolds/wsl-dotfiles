c9053c9a210128f211bd002c64f6288b3f26db47
Stephen Reynolds <mail@stephenreynolds.dev>
2022-12-23 03:56:36 -0500

    Customize LunarVim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,64 +1,15 @@
---[[mai
-lvim is the global options object
-
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
--- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
-
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = false
-lvim.colorscheme = "kanagawa"
--- to disable icons and use a minimalist setup, uncomment the following
--- lvim.use_icons = false
+lvim.colorscheme = "tokyonight-night"
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
--- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
--- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
--- unmap a default keymapping
--- vim.keymap.del("n", "<C-Up>")
--- override a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
-
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
--- local _, actions = pcall(require, "telescope.actions")
--- lvim.builtin.telescope.defaults.mappings = {
---   -- for input mode
---   i = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---     ["<C-n>"] = actions.cycle_history_next,
---     ["<C-p>"] = actions.cycle_history_prev,
---   },
---   -- for normal mode
---   n = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---   },
--- }
-
--- Change theme settings
--- lvim.builtin.theme.options.dim_inactive = true
--- lvim.builtin.theme.options.style = "storm"
 
 -- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
--- lvim.builtin.which_key.mappings["t"] = {
---   name = "+Trouble",
---   r = { "<cmd>Trouble lsp_references<cr>", "References" },
---   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
---   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
---   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
--- }
+lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
@@ -69,19 +20,19 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-    "bash",
-    "c",
-    "javascript",
-    "json",
-    "lua",
-    "python",
-    "typescript",
-    "tsx",
-    "css",
-    "rust",
-    "java",
-    "yaml",
-    "toml"
+	"bash",
+	"c",
+	"javascript",
+	"json",
+	"lua",
+	"python",
+	"typescript",
+	"tsx",
+	"css",
+	"rust",
+	"java",
+	"yaml",
+	"toml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -89,74 +40,21 @@ lvim.builtin.treesitter.highlight.enable = true
 lvim.builtin.dap.active = true
 
 -- generic LSP settings
-
--- -- make sure server will always be installed even if the server is in skipped_servers list
--- lvim.lsp.installer.setup.ensure_installed = {
---     "sumneko_lua",
---     "jsonls",
--- }
--- -- change UI setting of `LspInstallInfo`
--- -- see <https://github.com/williamboman/nvim-lsp-installer#default-configuration>
--- lvim.lsp.installer.setup.ui.check_outdated_servers_on_open = false
--- lvim.lsp.installer.setup.ui.border = "rounded"
--- lvim.lsp.installer.setup.ui.keymaps = {
---     uninstall_server = "d",
---     toggle_server_expand = "o",
--- }
-
--- ---@usage disable automatic installation of servers
--- lvim.lsp.installer.setup.automatic_installation = false
-
--- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
--- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust_analyzer" })
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
 
--- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
--- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
--- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
---   return server ~= "emmet_ls"
--- end, lvim.lsp.automatic_configuration.skipped_servers)
-
--- -- you can set a custom on_attach function that will be used for all the language servers
--- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
-
--- -- set a formatter, this will override the language server formatting capabilities (if it exists)
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "black", filetypes = { "python" } },
---   { command = "isort", filetypes = { "python" } },
---   {
---     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
---     command = "prettier",
---     ---@usage arguments to pass to the formatter
---     -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
---     extra_args = { "--print-with", "100" },
---     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
---     filetypes = { "typescript", "typescriptreact" },
---   },
--- }
 local formatters = require("lvim.lsp.null-ls.formatters")
-formatters.setup {
-    { command = "stylua", filetypes = { "lua" } }
-}
+formatters.setup({
+	{ command = "stylua", filetypes = { "lua" } },
+})
 
 local mason_path = vim.fn.glob(vim.fn.stdpath("data") .. "/mason")
 local codelldb_adapter = {
-    type = "server",
-    port = "${port}",
-    executable = {
-        command = mason_path .. "bin/codelldb",
-        args = { "--port", "${port}" }
-    }
+	type = "server",
+	port = "${port}",
+	executable = {
+		command = mason_path .. "bin/codelldb",
+		args = { "--port", "${port}" },
+	},
 }
 -- -- set additional linters
 -- local linters = require "lvim.lsp.null-ls.linters"
@@ -175,57 +73,93 @@ local codelldb_adapter = {
 --     filetypes = { "javascript", "python" },
 --   },
 -- }
--- TODO: wat
 -- Additional Plugins
 lvim.plugins = {
-    { "rebelot/kanagawa.nvim" },
-    { "ray-x/lsp_signature.nvim" },
-    { "norcalli/nvim-colorizer.lua",
-        config = function()
-            require("colorizer").setup({ "css", "scss", "html", "javascript", "lua" }, {
-                RGB = true, -- #RGB hex codes
-                RRGGBB = true, -- #RRGGBB hex codes
-                RRGGBBAA = true, -- #RRGGBBAA hex codes
-                rgb_fn = true, -- CSS rgb() and rgba() functions
-                hsl_fn = true, -- CSS hsl() and hsla() functions
-                css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-                css_fn = true -- Enable all CSS *functions*: rgb_fn, hsl_fn
-            })
-        end
-    },
-    { "folke/trouble.nvim",
-        cmd = "TroubleToggle",
-    },
-    { "folke/todo-comments.nvim",
-        requires = "nvim-lua/plenary.nvim",
-        config = function()
-            require("todo-comments").setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            }
-        end
-    },
-    "simrat39/rust-tools.nvim",
-    {
-        "saecki/crates.nvim",
-        tag = "v0.3.0",
-        requires = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("crates").setup {
-                null_ls = {
-                    enabled = true,
-                    name = "crates.nvim",
-                },
-            }
-        end,
-    },
-    {
-        "j-hui/fidget.nvim",
-        config = function()
-            require("fidget").setup()
-        end,
-    }
+	{ "rebelot/kanagawa.nvim" },
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "BufRead",
+		config = function()
+			require("lsp_signature").on_attach()
+		end,
+	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({ "css", "scss", "html", "javascript", "lua" }, {
+				RGB = true, -- #RGB hex codes
+				RRGGBB = true, -- #RRGGBB hex codes
+				RRGGBBAA = true, -- #RRGGBBAA hex codes
+				rgb_fn = true, -- CSS rgb() and rgba() functions
+				hsl_fn = true, -- CSS hsl() and hsla() functions
+				css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+				css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+			})
+		end,
+	},
+	{ "folke/trouble.nvim", cmd = "TroubleToggle" },
+	{
+		"folke/todo-comments.nvim",
+		requires = "nvim-lua/plenary.nvim",
+		config = function()
+			require("todo-comments").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	},
+	"simrat39/rust-tools.nvim",
+	{
+		"saecki/crates.nvim",
+		tag = "v0.3.0",
+		requires = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("crates").setup({
+				null_ls = {
+					enabled = true,
+					name = "crates.nvim",
+				},
+			})
+		end,
+	},
+	{
+		"j-hui/fidget.nvim",
+		config = function()
+			require("fidget").setup()
+		end,
+	},
+	"ThePrimeagen/harpoon",
+	"mbbill/undotree",
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
+	{
+		"sindrets/diffview.nvim",
+		event = "BufRead",
+	},
+	{
+		"tpope/vim-fugitive",
+		cmd = {
+			"G",
+			"Git",
+			"Gdiffsplit",
+			"Gread",
+			"Gwrite",
+			"Ggrep",
+			"GMove",
+			"GDelete",
+			"GBrowse",
+			"GRemove",
+			"GRename",
+			"Glgrep",
+			"Gedit",
+		},
+		ft = { "fugitive" },
+	},
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
@@ -243,123 +177,168 @@ lvim.plugins = {
 -- })
 
 pcall(function()
-    require("rust-tools").setup {
-        tools = {
-            executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
-            reload_workspace_from_cargo_toml = true,
-            runnables = {
-                use_telescope = true,
-            },
-            inlay_hints = {
-                auto = true,
-                only_current_line = false,
-                show_parameter_hints = false,
-                parameter_hints_prefix = "<-",
-                other_hints_prefix = "=>",
-                max_len_align = false,
-                max_len_align_padding = 1,
-                right_align = false,
-                right_align_padding = 7,
-                highlight = "Comment",
-            },
-            hover_actions = {
-                border = "rounded",
-            },
-            on_initialized = function()
-                vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
-                    pattern = { "*.rs" },
-                    callback = function()
-                        local _, _ = pcall(vim.lsp.codelens.refresh)
-                    end,
-                })
-            end,
-        },
-        dap = {
-            adapter = codelldb_adapter,
-        },
-        server = {
-            on_attach = function(client, bufnr)
-                require("lvim.lsp").common_on_attach(client, bufnr)
-                local rt = require "rust-tools"
-                vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
-            end,
+	require("rust-tools").setup({
+		tools = {
+			executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
+			reload_workspace_from_cargo_toml = true,
+			runnables = {
+				use_telescope = true,
+			},
+			inlay_hints = {
+				auto = true,
+				only_current_line = false,
+				show_parameter_hints = false,
+				parameter_hints_prefix = "<-",
+				other_hints_prefix = "=>",
+				max_len_align = false,
+				max_len_align_padding = 1,
+				right_align = false,
+				right_align_padding = 7,
+				highlight = "Comment",
+			},
+			hover_actions = {
+				border = "rounded",
+			},
+			on_initialized = function()
+				vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
+					pattern = { "*.rs" },
+					callback = function()
+						local _, _ = pcall(vim.lsp.codelens.refresh)
+					end,
+				})
+			end,
+		},
+		dap = {
+			adapter = codelldb_adapter,
+		},
+		server = {
+			on_attach = function(client, bufnr)
+				require("lvim.lsp").common_on_attach(client, bufnr)
+				local rt = require("rust-tools")
+				vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+			end,
 
-            capabilities = require("lvim.lsp").common_capabilities(),
-            settings = {
-                ["rust-analyzer"] = {
-                    lens = {
-                        enable = true,
-                    },
-                    checkOnSave = {
-                        enable = true,
-                        command = "clippy"
-                    }
-                }
-            }
-        }
-    }
+			capabilities = require("lvim.lsp").common_capabilities(),
+			settings = {
+				["rust-analyzer"] = {
+					lens = {
+						enable = true,
+					},
+					checkOnSave = {
+						enable = true,
+						command = "clippy",
+					},
+				},
+			},
+		},
+	})
 end)
 
 -- CHANGED --
 lvim.builtin.dap.on_config_done = function(dap)
-    dap.adapters.codelldb = codelldb_adapter
-    dap.configurations.rust = {
-        {
-            name = "Launch file",
-            type = "codelldb",
-            request = "launch",
-            program = function()
-                return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-            end,
-            cwd = "${workspaceFolder}",
-            stopOnEntry = false,
-        },
-    }
+	dap.adapters.codelldb = codelldb_adapter
+	dap.configurations.rust = {
+		{
+			name = "Launch file",
+			type = "codelldb",
+			request = "launch",
+			program = function()
+				return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+			end,
+			cwd = "${workspaceFolder}",
+			stopOnEntry = false,
+		},
+	}
 end
 
 vim.api.nvim_set_keymap("n", "<m-d>", "<cmd>RustOpenExternalDocs<Cr>", { noremap = true, silent = true })
 
 lvim.builtin.which_key.mappings["R"] = {
-    name = "Rust",
-    r = { "<cmd>RustRunnables<Cr>", "Runnables" },
-    t = { "<cmd>lua _CARGO_TEST()<cr>", "Cargo Test" },
-    m = { "<cmd>RustExpandMacro<Cr>", "Expand Macro" },
-    c = { "<cmd>RustOpenCargo<Cr>", "Open Cargo" },
-    p = { "<cmd>RustParentModule<Cr>", "Parent Module" },
-    d = { "<cmd>RustDebuggables<Cr>", "Debuggables" },
-    v = { "<cmd>RustViewCrateGraph<Cr>", "View Crate Graph" },
-    R = {
-        "<cmd>lua require('rust-tools/workspace_refresh')._reload_workspace_from_cargo_toml()<Cr>",
-        "Reload Workspace",
-    },
-    o = { "<cmd>RustOpenExternalDocs<Cr>", "Open External Docs" }
+	name = "Rust",
+	r = { "<cmd>RustRunnables<Cr>", "Runnables" },
+	t = { "<cmd>lua _CARGO_TEST()<cr>", "Cargo Test" },
+	m = { "<cmd>RustExpandMacro<Cr>", "Expand Macro" },
+	c = { "<cmd>RustOpenCargo<Cr>", "Open Cargo" },
+	p = { "<cmd>RustParentModule<Cr>", "Parent Module" },
+	d = { "<cmd>RustDebuggables<Cr>", "Debuggables" },
+	v = { "<cmd>RustViewCrateGraph<Cr>", "View Crate Graph" },
+	R = {
+		"<cmd>lua require('rust-tools/workspace_refresh')._reload_workspace_from_cargo_toml()<Cr>",
+		"Reload Workspace",
+	},
+	o = { "<cmd>RustOpenExternalDocs<Cr>", "Open External Docs" },
 }
 
 lvim.transparent_window = true
 vim.opt.relativenumber = true
 
 lvim.builtin.which_key.mappings["t"] = {
-    name = "Diagnostics",
-    t = { "<cmd>TroubleToggle<cr>", "trouble" },
-    w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "workspace" },
-    d = { "<cmd>TroubleToggle document_diagnostics<cr>", "document" },
-    q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
-    l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
-    r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
+	name = "Diagnostics",
+	t = { "<cmd>TroubleToggle<cr>", "trouble" },
+	w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "workspace" },
+	d = { "<cmd>TroubleToggle document_diagnostics<cr>", "document" },
+	q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
+	l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
+	r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
 }
 
 local util = require("lspconfig/util")
-require("lspconfig").hls.setup {
-    root_dir = function(filepath)
-        return util.root_pattern('hie.yaml', 'stack.yaml', 'cabal.project', "*.cabal", "package.yaml")(filepath)
-            or util.path.dirname(filepath)
-    end
-}
+require("lspconfig").hls.setup({
+	root_dir = function(filepath)
+		return util.root_pattern("hie.yaml", "stack.yaml", "cabal.project", "*.cabal", "package.yaml")(filepath)
+			or util.path.dirname(filepath)
+	end,
+})
 
 -- Tab size
-vim.bo.tabstop = 4
-vim.bo.shiftwidth = 4
-vim.bo.softtabstop = 4
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
 
 -- Disable mouse
--- vim.opt.mouse = ""
+vim.opt.mouse = ""
+
+vim.opt.title = false
+
+vim.opt.updatetime = 50
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+-- Harpoon
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+lvim.builtin.which_key.mappings["a"] = { mark.add_file, "Add to Harpoon" }
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<C-h>", function()
+	ui.nav_file(1)
+end)
+vim.keymap.set("n", "<C-j>", function()
+	ui.nav_file(2)
+end)
+vim.keymap.set("n", "<C-k>", function()
+	ui.nav_file(3)
+end)
+vim.keymap.set("n", "<C-l>", function()
+	ui.nav_file(4)
+end)
+
+-- Undo tree
+lvim.builtin.which_key.mappings["u"] = { vim.cmd.UndotreeToggle, "Undo Tree" }
+
+-- Remaps
+-- Move selected lines
+vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv")
+
+-- Move up/down half page while keeping cursor centered
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Move between search results while keeping cursor centered
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
